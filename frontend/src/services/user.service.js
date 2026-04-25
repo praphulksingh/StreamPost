9562ba0bb7e30137539c9d808d0189fbb842b491
Praphul <praphulsingh1712@gmail.com>
2026-04-26 00:01:56 +0530

    fixed watch history

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -4,10 +4,10 @@ export const userService = {
   getChannelProfile: async (username) => {
     const response = await api.get(`/users/c/${username}`);
     return response.data;
+  },
+  getWatchHistory: async () => {
+    const response = await api.get("/users/history");
+    return response.data;
   }
 };
 
-getWatchHistory: async () => {
-    const response = await api.get("/users/history");
-    return response.data;
-}
